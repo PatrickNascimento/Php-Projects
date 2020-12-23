cdd95f2607091009835a596718284e46b4615ef5
Patrick Souza do nascimento <psn1462@gmail.com>
2020-12-23 10:54:27 -0300

    Create ClassObject.php

---
+++ POO/POO/Elearn01/ClassObject.php
@@ -0,0 +1,26 @@
+<?php
+#classe Notífica.php
+
+class noticias {
+
+    public $titulo;
+    public $texto;
+
+    function setTitulo($valor){
+        $this->titulo = $valor;
+    }
+    function setTexto($valor){
+        $this->titulo = $valor;
+    }
+
+    function exibeNoticias(){        
+        echo $this->titulo. '<br>' ;
+        echo $this->texto;
+    }
+    
+}
+
+$noticia = new noticias();
+$noticia->titulo = "Hello World";
+$noticia->texto = "Orientação a Objetos com PHP";
+$noticia->exibeNoticias();
