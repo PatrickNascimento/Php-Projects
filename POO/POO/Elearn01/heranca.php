583e2739df3d84c739fd88a26fbbc566379471c6
Patrick Souza do nascimento <psn1462@gmail.com>
2020-12-23 11:29:26 -0300

    Herança

---
+++ POO/POO/Elearn01/heranca.php
@@ -0,0 +1,27 @@
+<?php
+include_once("ClassObject.php");
+
+Class heranca extends noticias{
+
+    public $image;
+
+    function setImage($valor){
+        $this->image = $valor;
+    }
+
+    function exibeNoticias()
+    {
+        echo $this->titulo ."<br>";
+        echo $this->texto . "<br>";
+        echo "<img src=\"". $this->image .">";
+    }
+}
+
+$noticia = new heranca;
+
+$noticia->titulo .= "Php";
+$noticia->texto .= "Heranca";
+$noticia->image = "php.jpg";
+
+$noticia->exibeNoticias();
+
