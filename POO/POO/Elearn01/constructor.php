03754cbc4f8016934bb22bae43e1fd1c100fc1f7
Patrick Souza do nascimento <psn1462@gmail.com>
2020-12-24 08:08:47 -0300

    Criando constructor
    Exemplo do constructor com PHP

---
+++ POO/POO/Elearn01/constructor.php
@@ -0,0 +1,24 @@
+<?php
+#Noticia constructor
+
+class noticia
+{
+    public $titulo;
+    public $texto;
+
+    function __construct($val_tit, $val_txt)
+    {
+    $this->titulo = $val_tit;    
+    $this->texto = $val_txt;    
+    }
+
+    function exibenoticias()
+    {
+        echo $this->titulo;
+        echo $this->texto;
+
+    }
+}
+
+$noticia = new noticia('Titulo constructor','exemplo de aplicação do constructor');
+$noticia->exibenoticias();
