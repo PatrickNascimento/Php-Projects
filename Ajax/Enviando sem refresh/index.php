1174abbaf8493a033337a7a697fea61cc92eac1d
Patrick Souza do nascimento <psn1462@gmail.com>
2021-01-19 10:29:14 -0300

    update ajax

--- Ajax/Enviando sem refresh/index.php
+++ Ajax/Enviando sem refresh/index.php
@@ -1,8 +1,23 @@
 <html>
   <head>
-  <link rel="stylesheet" href="style.css">
+      <style>
+          .form {
+            height: 300px;
+            width: 30%;
+            padding-left: 50px;
+            padding-right: 50px;
+            padding-top: 50px;
+            padding-bottom: 50px;
+            background-color: Gainsboro;
+          }
+      </Style>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">  
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script>
+    <script>    
+    $( document ).ready(function() {
+        $(".alert alert-success").hide();
+    });      
+
       $(function () {
         $('form').on('submit', function (e) {
           e.preventDefault();
@@ -10,8 +25,8 @@
             type: 'post',
             url: 'rota.php',
             data: $('form').serialize(),
-            success: function () {
-              alert('Dados enviados com sucesso!');
+            success: function () {              
+              $('#msg').show();
             }
           });
         });
@@ -19,15 +34,17 @@
     </script>
   </head>
   <body>
-  <div class="form-style-5">
+  <div class="form">
     <form>
     <label ty>Nome</label>
-      <input type="text"  name="nome" value="patrick"><br>
+      <input class="form-control" type="text"  name="nome" value=""><br>
       <label>email</label>
-      <input type="email" name="email" value="patrick"><br>
-      <input name="submit" type="submit" value="Submit">
-    </form>
-    <div class="send" style="display: none;"><p>ENVIADO</p></div>    
+      <input class="form-control" type="email" name="email" value=""><br>
+      <input  class="btn btn-primary" name="submit" type="submit" value="Submit">
+    </form>    
+  <div id="msg" class="alert alert-success" style="display :none">
+    <strong>Successo!</strong> Dados Enviados com sucesso!
+  </div>
     </div>
   </body>
 </html>
